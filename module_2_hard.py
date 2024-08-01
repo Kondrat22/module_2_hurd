4b8a7bf8a8ef031b6b186de6dc8c0c52155b539f
Kondrat22 <kondrat_va@bk.ru>
2024-08-01 22:37:30 +0300

    Update module_2_hard.py

--- module_2_hard.py
+++ module_2_hard.py
@@ -1,10 +1,3 @@
-# list_ = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-# for i in range(len(list_)):
-#    for j in range(1, 19):
-#    result =
-#    print()
-
-
 def generate_password(n):
     result = ""
     for i in range(1, n):
@@ -20,4 +13,4 @@ while number < 3 or number > 20:
 
 password = generate_password(number)
 print(f"Пароль для числа {number}: {password}")
-print("Поздравляю,вы сбежали")
+print("Вы сбежали")
